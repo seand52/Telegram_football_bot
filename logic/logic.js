7f831dbc37217f93a1d661285a8d111465b5d54c
Sean <seand52@gmail.com>
2019-10-19 11:35:42 +0200

    fix time +2 hours

--- logic/logic.js
+++ logic/logic.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
-const axios = require('axios');
-const moment = require('moment')
+const axios = require("axios");
+const moment = require("moment");
 
 class Logic {
   similarity(s1, s2) {
@@ -14,61 +14,76 @@ class Logic {
     if (longerLength == 0) {
       return 1.0;
     }
-    const test = (longerLength - this.editDistance(longer, shorter)) / parseFloat(longerLength);
-    return test
+    const test =
+      (longerLength - this.editDistance(longer, shorter)) /
+      parseFloat(longerLength);
+    return test;
   }
 
   editDistance(s1, s2) {
     s1 = s1.toLowerCase();
     s2 = s2.toLowerCase();
-  
+
     var costs = new Array();
     for (var i = 0; i <= s1.length; i++) {
       var lastValue = i;
       for (var j = 0; j <= s2.length; j++) {
-        if (i == 0)
-          costs[j] = j;
+        if (i == 0) costs[j] = j;
         else {
           if (j > 0) {
             var newValue = costs[j - 1];
             if (s1.charAt(i - 1) != s2.charAt(j - 1))
-              newValue = Math.min(Math.min(newValue, lastValue),
-                costs[j]) + 1;
+              newValue = Math.min(Math.min(newValue, lastValue), costs[j]) + 1;
             costs[j - 1] = lastValue;
             lastValue = newValue;
           }
         }
       }
-      if (i > 0)
-        costs[s2.length] = lastValue;
+      if (i > 0) costs[s2.length] = lastValue;
     }
     return costs[s2.length];
   }
 
   fetchData(id, numberOfMatches) {
-
-    const currentDate = moment()
+    const currentDate = moment();
     return axios({
-      method: 'get',
+      method: "get",
       url: `https://api.football-data.org/v2/teams/${id}/matches`,
       headers: {
-        'Content-Type': 'application/json',
-        'X-Auth-Token': `${process.env.FOOTBALL_API_TOKEN}`
+        "Content-Type": "application/json",
+        "X-Auth-Token": `${process.env.FOOTBALL_API_TOKEN}`
       }
     })
-    .then(res => {
-      const {matches} = res.data
-      const futureFatches = matches.filter(item => moment.utc(item.utcDate).diff(currentDate, 'minutes') > 0);
-      const nextMatches = futureFatches.slice(0, numberOfMatches)
-      if (nextMatches.length === 1) {
-      return `The next match is ${nextMatches[0].homeTeam.name} vs ${nextMatches[0].awayTeam.name} on ${moment.utc(nextMatches[0].utcDate).local().format('LLLL')} (GMT +2), good luck!`
-      }
-      const messagesArray = nextMatches.map(item => `-${item.homeTeam.name} vs ${item.awayTeam.name} on ${moment.utc(item.utcDate).local().format('LLLL')} (GMT + 2)`)
-      return `The next match es are:\n${messagesArray.join('\n')} \n good luck!`
-    })
-    .catch(err => {
-      return 'Sorry, I was not able to find the upcoming matches!'
-    })
+      .then(res => {
+        const { matches } = res.data;
+        const futureFatches = matches.filter(
+          item => moment.utc(item.utcDate).diff(currentDate, "minutes") > 0
+        );
+        const nextMatches = futureFatches.slice(0, numberOfMatches);
+        if (nextMatches.length === 1) {
+          return `The next match is ${nextMatches[0].homeTeam.name} vs ${
+            nextMatches[0].awayTeam.name
+          } on ${moment
+            .utc(nextMatches[0].utcDate)
+            .local()
+            .add(2, "hours")
+            .format("LLLL")} (GMT +2), good luck!`;
+        }
+        const messagesArray = nextMatches.map(
+          item =>
+            `-${item.homeTeam.name} vs ${item.awayTeam.name} on ${moment
+              .utc(item.utcDate)
+              .local()
+              .add(2, "hours")
+              .format("LLLL")} (GMT + 2)`
+        );
+        return `The next match es are:\n${messagesArray.join(
+          "\n"
+        )} \n good luck!`;
+      })
+      .catch(err => {
+        return "Sorry, I was not able to find the upcoming matches!";
+      });
   }
 }
 
